eb4f920f8d83769590cd6018bf0ee05cab36e918
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2024-06-13 18:40:01 +0300

    init: add basic structure of project

---
+++ kinopoisk/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <main>
+        <Routes></Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
